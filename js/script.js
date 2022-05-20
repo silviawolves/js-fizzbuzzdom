c47f60109dbadcb7d32ed6aa564bf6949b9c8612
silviawolves <silviacanepa.w@gmail.com>
2022-05-20 14:41:18 +0200

    multipli

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 "use strict"
 
 for (let i = 0; i <= 100; i++) {
-    console.log(i)
+
+    if (i % 3 === 0) {
+        const Fizz = (i % 3 === 0)
+        console.log(i, Fizz)
+    } else if (i % 5 === 0) {
+        const Buzz = (i % 5 === 0)
+        console.log(i, Buzz)
+    }
+
+
 }
 
 
